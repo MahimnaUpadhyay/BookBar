d021b5bb80a4d3da0691809f1bbb58d03b448f0b
mahimna upadhyay <upadhyaymahi28@gmail.com>
2024-10-26 13:50:11 +0530

    small changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      DB_URL: process.env.DB_URL,
+      DB_API_KEY: process.env.DB_API_KEY,
+    },
+  };
+  
+  export default nextConfig;
+  
